a73a68e473d3e29af07c640d8b20eba291f33abd
removedporn <86824510+removedporn@users.noreply.github.com>
2022-09-22 21:56:28 +0800

    Delete data-raw directory

--- data-raw/synth_dataset.R
+++
@@ -1,5 +0,0 @@
-rem_synthdata <- read.csv('data-raw/sythndata.csv', header = TRUE, sep = ',')
-
-#rem_synthdata[is.na(rem_synthdata$yr_pmt),'yr_pmt'] <- 0
-
-devtools::use_data(rem_synthdata,overwrite=TRUE)
